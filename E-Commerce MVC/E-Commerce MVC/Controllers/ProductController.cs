e8d3959224ec95cb541a7ba993ff70d93016a767
TrungTinns <trungtin073@gmail.com>
2024-09-23 22:43:56 +0700

    custom product card and import img

--- E-Commerce MVC/E-Commerce MVC/Controllers/ProductController.cs
+++ E-Commerce MVC/E-Commerce MVC/Controllers/ProductController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce_MVC.Data;
+using E_Commerce_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_MVC.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly TeeShopContext db;
+
+        public ProductController(TeeShopContext context)
+        {
+            db = context;
+        }
         public IActionResult Index(int? category)
         {
-            return View();
+            var products = db.HangHoas.AsQueryable();
+            if (category.HasValue) 
+            { 
+                products = products.Where(p => p.MaLoai == category.Value);
+            }
+
+            var result = products.Select(p => new ProductVM
+            {
+                ProductId = p.MaHh,
+                ProductName = p.TenHh,
+                Price = p.DonGia ?? 0.0,
+                Image = p.Hinh ?? "",
+                Description = p.MoTaDonVi ?? "",
+                ProductType = p.MaLoaiNavigation.TenLoai,
+            });
+            return View(result);
         }
     }
 }
---
+++ E-Commerce MVC/E-Commerce MVC/ViewModels/ProductVM.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Commerce_MVC.ViewModels
+{
+    public class ProductVM
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string ProductType { get; set; }
+    }
+}
