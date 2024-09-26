bb8dabf65fbdd6fc92e5eaca0ed4134416a9663e
TrungTinns <trungtin073@gmail.com>
2024-09-26 17:51:09 +0700

    signup UI

---
+++ E-Commerce MVC/E-Commerce MVC/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using E_Commerce_MVC.Data;
+using E_Commerce_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+
+namespace E_Commerce_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly TeeShopContext db;
+
+        public UserController(TeeShopContext context) 
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var users = model;
+            }
+            return View();
+        }
+    }
+}
---
+++ E-Commerce MVC/E-Commerce MVC/ViewModels/RegisterVM.cs
@@ -0,0 +1,43 @@
+﻿using E_Commerce_MVC.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_MVC.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "*")]
+        [MaxLength(20, ErrorMessage = "Must be 6-20 characters")]
+        public string UserID { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Fullname")]
+        [Required(ErrorMessage = "*")]
+        [MaxLength(50, ErrorMessage = "Must be 50 characters")]
+        public string FullName { get; set; }
+
+        public bool Gender { get; set; } = true;
+
+        [Display(Name = "Birth date")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+        [Display(Name = "Address")]
+        [MaxLength(60, ErrorMessage = "Must be 60 characters")]
+        public string Address { get; set; }
+
+        [Display(Name = "Phone number")]
+        [MaxLength(10, ErrorMessage = "Must be 10 numbers")]
+        [RegularExpression(@"0[987654321]\d{8}", ErrorMessage = "Invalid phone number")]
+        public string Phone { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public string Email { get; set; }
+
+        public string? Image { get; set; }
+    }
+}
