7312fc57b1514ea328e9aee6c3fb4e68d60ad41e
TrungTinns <trungtin073@gmail.com>
2024-10-14 23:48:32 +0700

    update checkout view

--- E-Commerce MVC/E-Commerce MVC/ViewModels/CheckoutVM.cs
+++ E-Commerce MVC/E-Commerce MVC/ViewModels/CheckoutVM.cs
@@ -6,6 +6,7 @@
         public string? FullName { get; set; }
         public string? Address { get; set; }
         public string? Phone { get; set; }
-        public string? Note { get; set; }
+/*        public string? DeliveryOption { get; set; }
+*/        public string? Note { get; set; }
     }
 }
